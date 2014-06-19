59e02bd7a9030f681b57578b9391623f7b4d415c
Nicholas Bollweg (Nick) <nick.bollweg@gmail.com>
2014-06-18 22:15:15 -0400

    #1: moving asset pattern

--- {{cookiecutter.repo_name}}/{{cookiecutter.pkg_name}}/widgets/{{cookiecutter.widget_file}}.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.pkg_name}}/widgets/{{cookiecutter.widget_file}}.py
@@ -1,23 +1,27 @@
 # -*- coding: utf-8 -*-
 
-# Impor widget and traitlets
+import os
+
+# Import widgets, provisioners and traitlets
 from IPython.html import widgets
 from IPython.utils import traitlets
-from IPython.display import display, Javascript
 
+from .mixins import InstallerMixin
 
-class {{cookiecutter.widget_stem}}Widget(widgets.DOMWidget):
-    # A sample Widget
+class {{cookiecutter.widget_stem}}Widget(InstallerMixin, widgets.DOMWidget):
+    '''
+    A sample widget... with one "real" traitlet, and a bunch of housekeeping
+    '''
+
+    # the name of the Backbone.View subclass to be used
     _view_name = traitlets.Unicode('{{cookiecutter.widget_stem}}View', sync=True)
+
+    # an actual value, used in the front-end
     value = traitlets.Unicode(sync=True)
 
-    def __init__(self, *args, **kwargs):
-        # this could be moved into a common base class
-        script = '''IPython.load_extensions('%(path)s/%(view)s');''' % {
-            'path': '{{cookiecutter.pkg_name}}/js',
-            'view': '{{cookiecutter.widget_file}}'
-        }
-        display(Javascript(script, css=[
-            "/nbextensions/{{cookiecutter.pkg_name}}/css/{{cookiecutter.widget_file}}.css"
-        ]))
-        super({{cookiecutter.widget_stem}}Widget, self).__init__(*args, **kwargs)
+    # provisioning and loading JS/CSS assets
+    _view_static = os.path.join(
+        os.path.dirname(__file__), '..', 'static', '{{cookiecutter.pkg_name}}'
+    )
+    _view_module = 'js/{{cookiecutter.widget_file}}'
+    _view_style = 'css/{{cookiecutter.widget_file}}'
